dd616b0e77ca54d0d9f888479ed146958bffb4cf
fannarfent <fannarorn17@gmail.com>
2022-09-06 14:22:17 +0000

    gabagool

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-amo = "mjá"
-gorp = "myndavél"
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"+ amo + gorp
+    return "<p>Hello, World!</p>"
 
